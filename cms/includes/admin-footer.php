390ff3a4b11d310b1661a3c401ab94dfcac16b85
WKUkyungmoon <rhdrud567@wku.ac.kr>
2026-06-09 15:21:20 +0900

    add
    add

--- cms/includes/admin-footer.php
+++ cms/includes/admin-footer.php
@@ -6,3 +6,24 @@
     <script src="../js/site.js"></script>
   </body>
 </html>
+
+<script>
+const imageInput = document.getElementById('image');
+const preview = document.getElementById('preview');
+
+imageInput.addEventListener('change', function () {
+
+    const file = this.files[0];
+
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+        preview.src = e.target.result;
+        preview.style.display = "block";
+    };
+
+    reader.readAsDataURL(file);
+});
+</script>
